5852024e0df5496ff9f1aae1399807fa06c50bb5
aniketnitj <aniketnitj@gmail.com>
2016-09-26 18:08:43 +0530

    CORS issue

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.get('/', function(req, res){
+app.get('/', function(req, res,next){
   //send the index.html file for all requests
   res.sendFile(__dirname + '/index.html');
 
