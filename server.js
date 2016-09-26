d6936c473cfd81a79f750fa559de0a76f4504a7c
aniketnitj <aniketnitj@gmail.com>
2016-09-26 19:33:55 +0530

    CORS

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ io.set('origins', '*:*');
 
 var port = process.env.PORT || 3151;
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Origin','*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Expose-Headers', 'Content-Length');
